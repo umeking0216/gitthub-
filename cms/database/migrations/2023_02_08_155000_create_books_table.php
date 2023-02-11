00d4c87e5a9a4ff9dd989fb0e9a35b190e68a66d
EC2 Default User <ec2-user@ip-172-31-33-135.ec2.internal>
2023-02-11 01:36:56 +0000

    初回

---
+++ cms/database/migrations/2023_02_08_155000_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+             $table->id();
+             $table->string('item_name'); //追加
+             $table->integer('item_number'); //追加
+             $table->integer('item_amount'); //追加
+             $table->datetime('published'); //追加
+             $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ cms/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController; //追加
+use App\Models\Book; //追加
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//本：ダッシュボード表示(books.blade.php)
+Route::get('/', [BookController::class,'index'])->middleware(['auth'])->name('book_index');
+Route::get('/dashboard', [BookController::class,'index'])->middleware(['auth'])->name('dashboard');
+
+//本：追加 
+Route::post('/books',[BookController::class,"store"])->name('book_store');
+
+//本：削除 
+Route::delete('/book/{book}', [BookController::class,"destroy"])->name('book_destroy');
+
+//本：更新画面
+Route::post('/booksedit/{book}',[BookController::class,"edit"])->name('book_edit'); //通常
+Route::get('/booksedit/{book}', [BookController::class,"edit"])->name('edit');      //Validationエラーありの場合
+
+//本：更新画面
+Route::post('/books/update',[BookController::class,"update"])->name('book_update');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
